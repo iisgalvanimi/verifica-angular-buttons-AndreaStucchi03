3ceada6355b32c4351938cfa8bb2feae30c88e99
AndreaStucchi03 <stucchi.andrea@iisgalvanimi.edu.it>
2022-03-17 08:59:46 +0000

    Finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+  nombtn=""
+
+  list: string[] = []
+  testo1:string="button1";
+  testo2:string="button2";
+  testo3:string="button3";
+  testo4:string="button4";
+
+  onClick1(){
+    this.nombtn = "Button1"
+    this.list.push(this.testo1)
+  }
+
+  onClick2(){
+    this.nombtn = "Button2"
+    this.list.push(this.testo2)
+  }
+
+  onClick3(){
+    this.nombtn = "Button3"
+    this.list.push(this.testo3)
+  }
+
+  onClick4(){
+    this.nombtn = "Button4"
+    this.list.push(this.testo4)
+  }
+
 }
